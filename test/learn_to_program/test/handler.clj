fcbdefa0b777455b6852ddf9e81c5b2b181f7559
Matt Burns <mburns@dandb.com>
2014-07-07 10:31:16 -0700

    remove-cruft: getting rid of lots of obvious unecessary files

--- test/learn_to_program/test/handler.clj
+++
@@ -1,7 +0,0 @@
-(ns learn-to-program.test.handler
-  (:require [clojure.test :refer :all]
-            [learn-to-program.handler :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
