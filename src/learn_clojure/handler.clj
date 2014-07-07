c7b6f272520c5b04874907fa3dfc360a32c3464b
Matt Burns <mburns@dandb.com>
2014-07-07 10:54:19 -0700

    remove-cruft: eleminated lots of useless javascript and css renamed the site css file

--- src/learn_clojure/handler.clj
+++ src/learn_clojure/handler.clj
@@ -1,10 +1,10 @@
 (ns learn-clojure.handler
-  (:require [net.cgrand.enlive-html :refer [deftemplate clone-for content add-class set-attr do->
-                                            html-content first-child nth-of-type defsnippet]]
-            [cheshire.core :refer :all]
+  (:require [cheshire.core :refer :all]
             [compojure.handler :as handler]
             [compojure.core :as core :refer [routes defroutes GET]]
             [compojure.route :as c-route :refer [resources files not-found]]
+            [net.cgrand.enlive-html :refer [deftemplate clone-for content add-class set-attr do->
+                                            html-content first-child nth-of-type defsnippet]]
             [ring.adapter.jetty :as ring :refer [run-jetty]]
             [ring.util.response :refer [file-response response]]
             [shoreleave.server-helpers :refer [safe-read]])
@@ -41,26 +41,6 @@
   [:a.brand] (content title)
   [:ul#nav-bar-dropdown-menu] (content (map #(menu-model % menu-item-model)  menu-data)))
 
-  (comment
-  [:div#navbar] (content (nav-bar {:title site-name :menu-data menu-data}))
-  [:ul.pages :li] (clone-for [p pages]
-                             [:a] (do->
-                                   (add-class "btn")
-                                   (add-class "btn-success")
-                                   (set-attr :href (str "#tab" (:order p)))
-                                   (set-attr :data-toggle "tab")
-                                   (content (:header p))))
-  [:div.tab-content :section.tab-pane] (clone-for [p pages]
-                                                  (do->
-                                               (set-attr :id (str "tab" (:order p)))
-                                               (html-content (:contents p))))
-  [[:ul.pages (nth-of-type 1)] :> first-child] (add-class "active")
-  [[:.tab-pane first-child]] (add-class "active")
-  )
-
-
-
-
 ;; HTML DOM
 (deftemplate index-page "templates/html/index.html"
   [{:keys [site-name pages menu-data] :as settings}]
@@ -68,18 +48,14 @@
 
 ;; View
 (defn render-str [t] (apply str t))
-;;(def render-to-response (comp response render-str))
-
-  ;;  (render-to-response (index-page {:site-name "MySiteNow!.com" :pages pages :menu-data menu}))
 
 (defn render-dom
   [{:keys [pages menu] :as args}]
-  (index-page {:site-name "MySiteNow!.com" :pages nil :menu-data menu}))
+  (index-page {:site-name "Clojure Lunch and Learn" :pages nil :menu-data menu}))
 
 ;; Controller/Router
 (defroutes app-routes
   (GET "/" [] (render-dom {:pages (:pages config) :menu (:drop_down_menu (parse-stream (clojure.java.io/reader "resources/menu.json") true)) }))
-  ;;  (GET "/" [] (render-dom ))
   (c-route/resources "/design/" {:root "templates/html"})
   (c-route/resources "/design/css/" {:root "public/css"})
   (c-route/resources "/css/" {:root "public/css"})
