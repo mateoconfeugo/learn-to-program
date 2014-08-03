33cb59b00acbc10a055b9b96bd0e2ef59fb868c9
Matt Burns <mburns@dandb.com>
2014-08-03 13:26:12 -0700

    edit application is working at a rudimentary level

--- src/learn_clojure/controller/api.clj
+++ src/learn_clojure/controller/api.clj
@@ -1,8 +1,12 @@
-(ns learn-clojure.controller.api)
+(ns learn-clojure.controller.api
+  (:require [learn-clojure.winery-data-bot :refer [mine]]))
 
+(def base-uri "http://pasowine.com/wineries")
 
-(defn do-value-added-proposition
-  "Make money here"
+(defn mine-wine-data-from-paso
+  "gets most of the relevant information about the various wines in paso robles"
   []
-  {:results []
-   :message "blah"})
+  (do
+    (mine {:uri base-uri :region "paso-robles"})
+      {:results []
+       :message "blah"}))
