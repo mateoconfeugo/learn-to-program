596746d02bef7dd5680426889bc858d58bdc36db
mateoconfeugo <matthewburns@gmail.com>
2014-10-11 17:56:20 +0000

    LEARNCLJ-8: added repl lecture for channels that will be part of lecture 6

--- src/learn_clojure/service/winery_data_bot.clj
+++ src/learn_clojure/service/winery_data_bot.clj
@@ -1,11 +1,21 @@
 (ns learn-clojure.service.winery-data-bot
-  (:require [clojure.core.async  :refer [put! >! chan go <!! alts!]]
-            [clojure.pprint :refer [pprint]]
-            [clojure.edn]
-            [com.ashafa.clutch :as clutch]
-            [net.cgrand.enlive-html :as html]
-            [clojure.core.match :refer [match]]
-            [clojurewerkz.urly.core :refer [query-of]]))
+  ^ {:author "Matthew Burns"
+     :doc "Contains the control flow logic for the asynchronous aspects of the program.
+   The control logic of the program is decoupled and isolated by using the core.async
+   go channels.
+   The various logical piecies of the processing system have channels that serve
+   as connection to each piece.
+   A convention of sending a vector container a keyword token message  pair is used
+   when inserting data into the channel system.
+   This pair of information is used by the the dispatcher to route the correct data to
+   correct handling function." }
+    (:require [clojure.core.async  :refer [put! >! chan go <!! alts!]]
+              [clojure.pprint :refer [pprint]]
+              [clojure.edn]
+              [com.ashafa.clutch :as clutch :refer [get-database with-db put-document]]
+              [net.cgrand.enlive-html :as html]
+              [clojure.core.match :refer [match]]
+              [clojurewerkz.urly.core :refer [query-of]]))
 
 (def base-uri "http://pasowine.com/wineries")
 
@@ -44,36 +54,59 @@
      :uri u
      :winery-id winery-id}))
 
-(defn mine
-  "Contains the control flow logic for the asynchronous aspects of the program"
+;; What are the advantages of using core async go channels?
+;; Why do I need to have access to the channels in the dispatch tables (one of the reasons for the closure) couldn't there be
+;; another channel that serves to route all the channels isn't that what alt! is already doing
+
+;; Maybe have control logic be a multimethod that determines which control logic to implement with a system
+;; The multimethods could all return a record that Implements the ChannelControlLogic Protocol
+(defn control-logic
+  "Control logic of a screen scraper get raw data -> filter/transform data -> store data"
   [{:keys [uri region] :as settings} ]
   (let [doc-db (wineries-doc-db region)
         ;; CHANNELS
         retreive-channel (chan)
         extract-channel (chan)
-        store-channel (chan)
-        channels [retreive-channel extract-channel store-channel]
-        ;; LAMBDA WIRING
-        retreive-fn (fn [uri]
-                      (let [id  (nth (first (re-seq #"winery=(\d+)" (query-of uri))) 1)
-                            d (fetch-uri uri)
-                            msg  {:dom d :winery-id id}]
-                        (go (>! extract-channel [:extract msg]))))
+        store-channel (chan)]
+    {:dispatcher (fn [ch tuple]
+                   (let [[msg-token data] (take 2 tuple)]
+                     (match [msg-token]
+                            [:retreive] ((fn [uri]
+                                           (let [winery-id  (nth (first (re-seq #"winery=(\d+)" (query-of uri))) 1)
+                                                 html (fetch-uri uri)
+                                                 msg  {:dom html :winery-id winery-id}]
+                                             (go (>! extract-channel [:extract msg])))) data)
 
-        extract-fn (fn [data] (go (>! store-channel [:store (extract-winery-data data)])))
-;;        store-fn  (fn [data]  (clutch/with-db doc-db (clutch/put-document data)))
-        store-fn  (fn [data]  (pprint data))
-        ;; DISTPATCH TABLE
-        dispatcher (fn [ch tuple]
-                     (let [[msg-token data] (take 2 tuple)]
-                       (match [msg-token]
-                              [:retreive] (retreive-fn data)
-                              [:extract] (extract-fn  data)
-                              [:store] (store-fn data))))]
-    (do
-      ;; PUT CHANNEL EVENTS INTO DISPATCHER
-      (go (while true
-            (let [[val ch] (alts! channels)]
-              (dispatcher ch val))))
-      ;; START PUSHING INPUT INTO SYSTEM
-      (map #(go (>! retreive-channel [:retreive %]))  (fetch-winery-list uri)) )))
+                            [:extract] ((fn [data] (go (>! store-channel [:store (extract-winery-data data)]))) data)
+
+                            [:store] ((fn [data] (with-db doc-db (put-document data))) data))))
+     :channels {:iput retreive-channel :process extract-channel  :store store-channel}}))
+
+
+ ;; Run system could be a macro that will take anything iplementing the ChannelControlLogic protocol
+(defn gather
+  [{:keys [uri region] :as settings} ]
+  (let [{:keys [dispatcher channels-map]} (control-logic settings)
+        channels (vals channels-map)
+        input (:input channels-map)]
+
+    ;; 1) starting handling messages flowing out of the alts! with the dispatcher
+    (do (go (while true
+              (let [[val ch] (alts! channels)]
+                (dispatcher ch val))))
+
+    ;; 2) input data into the system for processing
+        (map #(go (>! input [:retreive %])) (fetch-winery-list uri))
+
+    ;; 3) return the channel for further inputting
+        input)))
+
+;; It'd be cool to:
+;; 1) show some of the higher level things we can do with channels
+;; 2) Have some of the handlers be objects implementing a protocol, multimethod and normal functions
+;;   this should show how this can be used to glue things together
+
+(comment
+;;  (fetch-winery-list base-uri)
+;;  (gather {:uri base-uri :region "paso" } )
+  )
